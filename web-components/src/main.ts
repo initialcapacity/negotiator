a681f6ab91c645d5c7feaec1ecb46f0057b8d538
Tyson Gern <tyson@initialcapacity.io>
2023-07-11 21:58:40 -0600

    Move flashing dots

--- web-components/src/main.ts
+++ web-components/src/main.ts
@@ -1,4 +1,4 @@
-export * from './chat-input/flashing-dots.ts';
 export * from './chat-input/chat-input.ts';
+export * from './chat-messages/flashing-dots.ts';
 export * from './chat-messages/chat-messages.ts';
 export * from './negotiation-chat/negotiation-chat.ts';
