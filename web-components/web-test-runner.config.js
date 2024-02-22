5c808f74290f593eeed3bdad21a8d8ab78e279ab
Tyson Gern <tyson@initialcapacity.io>
2024-02-21 21:52:05 -0700

    Clean up test logs

--- web-components/web-test-runner.config.js
+++ web-components/web-test-runner.config.js
@@ -1,25 +1,42 @@
 import {vitePlugin} from '@remcovaes/web-test-runner-vite-plugin';
 import {endpoints} from './test/test-server-endpoints.js'
 
-export default {
-    plugins: [vitePlugin({
-        plugins: [{
-            name: 'test-server',
-            configureServer: server => {
-                server.middlewares.use(async ({url}, response, next) => {
-                    const endpoint = endpoints[url];
-                    if (endpoint === undefined) {
-                        next();
-                        return
-                    }
+const filteredLogs = [
+    'Lit is in dev mode. Not recommended for production! See https://lit.dev/msg/dev-mode for more information.',
+    '[vite] connecting',
+]
 
-                    if (endpoint.contentType) {
-                        response.setHeader("Content-Type", endpoint.contentType);
-                    }
-                    response.statusCode = endpoint.statusCode;
-                    response.end(endpoint.body);
-                });
+const filterBrowserLogs = (log) => {
+    for (const arg of log.args) {
+        if (typeof arg === 'string' && filteredLogs.some(l => arg.includes(l))) {
+            return false
+        }
+    }
+    return true
+}
+
+const testServerPlugin = {
+    name: 'test-server',
+    configureServer: server => {
+        server.middlewares.use(async ({url}, response, next) => {
+            const endpoint = endpoints[url];
+            if (endpoint === undefined) {
+                next();
+                return
             }
-        }]
+
+            if (endpoint.contentType) {
+                response.setHeader("Content-Type", endpoint.contentType);
+            }
+            response.statusCode = endpoint.statusCode;
+            response.end(endpoint.body);
+        });
+    }
+};
+export default {
+    filterBrowserLogs,
+    staticLogging: true,
+    plugins: [vitePlugin({
+        plugins: [testServerPlugin]
     })],
 };
